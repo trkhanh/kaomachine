0aad0491aa85a60b99781914ca4b68d0b428ff28
khanhtran <tran_khanh@outlook.com>
2020-01-25 21:12:55 +0700

    set up vsc- config

---
+++ .vsc/vsc_config.sh
@@ -0,0 +1,12 @@
+code --extensions-dir <dir>
+    Set the root path for extensions.
+code --list-extensions
+    List the installed extensions.
+code --show-versions
+    Show versions of installed extensions, when using --list-extension.
+code --install-extension (<extension-id> | <extension-vsix-path>)
+    Installs an extension.
+code --uninstall-extension (<extension-id> | <extension-vsix-path>)
+    Uninstalls an extension.
+code --enable-proposed-api (<extension-id>)
+    Enables proposed API features for extensions. Can receive one or more extension IDs to enable individually.
