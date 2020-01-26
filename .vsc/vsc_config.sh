4276b8e3153003dc165a84ce4eddf217dffce046
khanhtran <tran_khanh@outlook.com>
2020-01-26 09:53:44 +0700

    update key binding to Vim

--- .vsc/vsc_config.sh
+++ .vsc/vsc_config.sh
@@ -1,12 +1,16 @@
-code --extensions-dir <dir>
-    Set the root path for extensions.
+# VSC config
+
+#List the installed extensions.
 code --list-extensions
-    List the installed extensions.
+
+#Show versions of installed extensions, when using --list-extension.
 code --show-versions
-    Show versions of installed extensions, when using --list-extension.
-code --install-extension (<extension-id> | <extension-vsix-path>)
-    Installs an extension.
-code --uninstall-extension (<extension-id> | <extension-vsix-path>)
-    Uninstalls an extension.
-code --enable-proposed-api (<extension-id>)
-    Enables proposed API features for extensions. Can receive one or more extension IDs to enable individually.
+
+#Installs an extension.
+code --install-extension 
+
+#Uninstalls an extension.
+#code --uninstall-extension (<extension-id> | <extension-vsix-path>)
+
+#Enables proposed API features for extensions. Can receive one or more extension IDs to enable individually.
+#code --enable-proposed-api (<extension-id>)
---
+++ keybinding.sh
@@ -0,0 +1,4 @@
+#Set Vim style
+set -o vi
+#Set Emacs style ( default )
+#set -o emacs
